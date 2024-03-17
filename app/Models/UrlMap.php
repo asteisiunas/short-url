2d145e6fe0e7272f1cb90f0b666bdfabb048cd9c
Duadus <info@duadus.shop>
2024-03-17 17:23:36 +0200

    declare strict types

--- app/Models/UrlMap.php
+++ app/Models/UrlMap.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -7,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property null|int $id
  * @property null|string $short_url
+ * @method static whereNull(string $string)
  */
 class UrlMap extends Model
 {
